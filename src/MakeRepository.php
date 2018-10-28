00b27b47c65c14a3fbd53a6dc707ade943df150c
G Jason Sharma <34002768+Jason-Guru@users.noreply.github.com>
2018-10-28 11:47:05 +0530

    Add files via upload

---
+++ src/MakeRepository.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace JasonGuru\RepositoryGenerator;
+
+use Illuminate\Console\GeneratorCommand;
+
+class MakeRepository extends GeneratorCommand
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'make:repository {name}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Create a new repository class';
+
+    /**
+     * The type of class being generated.
+     *
+     * @var string
+     */
+    protected $type = 'Repository';
+
+    /**
+     * Determine if the class already exists.
+     *
+     * @param  string  $rawName
+     * @return bool
+     */
+    protected function alreadyExists($rawName)
+    {
+        return class_exists($rawName);
+    }
+
+    /**
+     * Get the stub file for the generator.
+     *
+     * @return string
+     */
+    protected function getStub()
+    {
+        return __DIR__.'/stubs/repository.stub';
+    }
+
+    /**
+     * Get the default namespace for the class.
+     *
+     * @param  string  $rootNamespace
+     * @return string
+     */
+    protected function getDefaultNamespace($rootNamespace)
+    {
+        return $rootNamespace.'\Repositories';
+    }
+}
---
+++ src/RepositoryServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace JasonGuru\RepositoryGenerator;
+
+use Illuminate\Support\ServiceProvider;
+
+class RepositoryServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->commands(MakeRepository::class);
+    }
+}
